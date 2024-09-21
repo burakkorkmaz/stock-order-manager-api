0a19e34958c43bdf91c245cd2786a4a2d884d06f
Burak Kağan Korkmaz <bkagankrkmz@gmail.com>
2024-09-21 21:10:49 +0300

    Initialized Stock Order Manager API for Spring Boot

---
+++ src/main/java/com/mybroker/stockordermanagerapi/StockOrderManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.mybroker.stockordermanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOrderManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockOrderManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybroker/stockordermanagerapi/StockOrderManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybroker.stockordermanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrderManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
